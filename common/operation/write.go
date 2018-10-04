f52b678ad6f7b5e754afc1a91ea530c0563db163
Just1689 <Just1689@gmail.com>
2018-10-04 20:40:41 +0200

    meh

--- common/operation/write.go
+++ common/operation/write.go
@@ -1,7 +1,5 @@
 package operation
 
-import "encoding/json"
-
 const (
 	WriteOperationV1Topic = "write-operation-v1"
 )
@@ -10,8 +8,4 @@ type WriteOperationV1 struct {
 	Table  string `json:"table"`
 	Method string `json:"method"`
 	Item   []byte `json:"item"`
-}
-
-func (i *WriteOperationV1) ToJsonBytes() ([]byte, error) {
-	return json.Marshal(i)
 }
