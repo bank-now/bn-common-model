336c8f63bc8e95e6b7e355163b77154976c74bdd
Just1689 <Just1689@gmail.com>
2018-10-04 20:36:23 +0200

    Write operation topic

--- common/operation/write.go
+++ common/operation/write.go
@@ -1,6 +1,10 @@
 package operation
 
-type WriteOperation struct {
+const (
+	WriteOperationV1Topic = "write-operation-v1"
+)
+
+type WriteOperationV1 struct {
 	Table  string `json:"table"`
 	Method string `json:"method"`
 	Item   []byte `json:"item"`
